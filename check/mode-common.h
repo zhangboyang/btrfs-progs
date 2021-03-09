zhangboyang/btrfs-progs

commit fc38ae7f4826f0cb0c668c1e044cab1fe624fe0b

btrfs-progs: check: detect and warn about tree blocks crossing 64K page boundary
For the incoming subpage support, there is a new requirement for tree
blocks.  Tree blocks should not cross 64K page boundary.

For current btrfs-progs and kernel, there shouldn't be any causes to
create such tree blocks.  But still, we want to detect such tree blocks
in the wild before subpage support fully lands in upstream.

This patch will add such check for both lowmem and original mode.
Currently it's just a warning, since there aren't many users using 64K
page size yet.

Signed-off-by: Qu Wenruo <wqu@suse.com>
Signed-off-by: David Sterba <dsterba@suse.com>

check/mode-common.h
@@ -174,4 +174,22 @@ static inline u32 btrfs_type_to_imode(u8 type)
 
 int get_extent_item_generation(u64 bytenr, u64 *gen_ret);
 
+/*
+ * Check tree block alignement for future subpage support on 64K page system.
+ *
+ * Subpage support on 64K page size require one eb to be completely contained
+ * by a page. Not allowing a tree block to cross 64K page boudanry.
+ *
+ * Since subpage support is still under development, this check only provides
+ * warning.
+ */
+static inline void btrfs_check_subpage_eb_alignment(u64 start, u32 len)
+{
+	if (start / BTRFS_MAX_METADATA_BLOCKSIZE !=
+	    (start + len) / BTRFS_MAX_METADATA_BLOCKSIZE)
+		warning(
+"tree block [%llu, %llu) crosses 64K page boudnary, may cause problem for 64K page system",
+			start, start + len);
+}
+
 #endif

check/main.c
@@ -5400,6 +5400,8 @@ static int process_extent_item(struct btrfs_root *root,
 		      num_bytes, gfs_info->sectorsize);
 		return -EIO;
 	}
+	if (metadata)
+		btrfs_check_subpage_eb_alignment(key.objectid, num_bytes);
 
 	memset(&tmpl, 0, sizeof(tmpl));
 	tmpl.start = key.objectid;

check/mode-lowmem.c
@@ -4264,6 +4264,8 @@ static int check_extent_item(struct btrfs_path *path)
 		      key.objectid, key.objectid + nodesize);
 		err |= CROSSING_STRIPE_BOUNDARY;
 	}
+	if (metadata)
+		btrfs_check_subpage_eb_alignment(key.objectid, nodesize);
 
 	ptr = (unsigned long)(ei + 1);
 

commit dc0dd43c3f061edffb6fb0c25ee6457aad507590

btrfs-progs: fix false alert on tree block crossing 64K page boundary
[BUG]
When btrfs-check is executed on even newly created fs, it can report
tree blocks crossing 64K page boundary like this:

  Opening filesystem to check...
  Checking filesystem on /dev/test/test
  UUID: 80d734c8-dcbc-411b-9623-a10bd9e7767f
  [1/7] checking root items
  [2/7] checking extents
  WARNING: tree block [30523392, 30539776) crosses 64K page boudnary, may cause problem for 64K page system
  [3/7] checking free space cache
  [4/7] checking fs roots
  [5/7] checking only csums items (without verifying data)
  [6/7] checking root refs
  [7/7] checking quota groups skipped (not enabled on this FS)
  found 131072 bytes used, no error found
  total csum bytes: 0
  total tree bytes: 131072
  total fs tree bytes: 32768
  total extent tree bytes: 16384
  btree space waste bytes: 125199
  file data blocks allocated: 0
   referenced 0

[CAUSE]
Tree block [30523392, 30539776) is at the last 16K slot of page.
As 30523392 % 65536 = 49152, and 30539776 % 65536 = 0.

The cross boundary check is using exclusive end, which causes false
alerts.

[FIX]
Use inclusive end to do the cross 64K boundary check.

Reported-by: Wang Yugui <wangyugui@e16-tech.com>
Issue: #352
Fixes: fc38ae7f4826 ("btrfs-progs: check: detect and warn about tree blocks crossing 64K page boundary")
Signed-off-by: Qu Wenruo <wqu@suse.com>
Signed-off-by: David Sterba <dsterba@suse.com>

check/mode-common.h
@@ -186,7 +186,7 @@ int get_extent_item_generation(u64 bytenr, u64 *gen_ret);
 static inline void btrfs_check_subpage_eb_alignment(u64 start, u32 len)
 {
 	if (start / BTRFS_MAX_METADATA_BLOCKSIZE !=
-	    (start + len) / BTRFS_MAX_METADATA_BLOCKSIZE)
+	    (start + len - 1) / BTRFS_MAX_METADATA_BLOCKSIZE)
 		warning(
 "tree block [%llu, %llu) crosses 64K page boudnary, may cause problem for 64K page system",
 			start, start + len);